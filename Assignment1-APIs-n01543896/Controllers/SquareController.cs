c460c00d05eb71012ea3c595bbbec10d7a90edc4
Vaibhav Baria <kiboyagami0@gmail.com>
2024-02-02 00:19:05 -0500

    question 2 done

---
+++ Assignment1-APIs-n01543896/Controllers/SquareController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment1_APIs_n01543896.Controllers
+{
+    public class SquareController : ApiController
+    {
+        /// <summary>
+        /// Returns the square  of the input number
+        /// </summary>
+        /// <param name="numb">The input number</param>
+        /// <returns>The square of the integer input</returns>
+        /// <example>
+        ///     GET /api/Square/4 -> 16
+        /// </example>
+        /// <example>
+        ///     GET /api/Square/10 -> 100
+        /// </example>
+        [Route("api/Square/{numb}")]
+        public int Get(int numb) {
+            int result = numb * numb;
+
+            return result;
+        }
+    }
+}
